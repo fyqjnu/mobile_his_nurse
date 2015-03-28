048be389df3da8f8668ea0a19f436836d00c1e74
fyqjnu <fyqjnu@qq.com>
2015-03-28 10:52:21 +0800

    1.添加drawable目录存放xml文件，添加home_item_selector.xml实现主界面按钮点击效果 2.完善主界面功能

--- src/com/his/nurse/activity/HomeActivity.java
+++ src/com/his/nurse/activity/HomeActivity.java
@@ -13,6 +13,7 @@ import android.graphics.RectF;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -32,6 +33,8 @@ public class HomeActivity extends BaseActivity implements OnClickListener {
 
     
     private Header header;
+    private PopupWindow pop;
+    private TextView tvTaskNum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,7 +72,7 @@ public class HomeActivity extends BaseActivity implements OnClickListener {
         llTimeContainerLayout.getLayoutParams().height = h;
         ILog.d(h);
         
-        Bitmap bg = createRoundBitmap(h);
+        Bitmap bg = createRoundBitmap(h, 0xcff6efdd);
         llTimeContainerLayout.setBackgroundDrawable(new BitmapDrawable(bg));
         
         ImageView ivTodayTask = (ImageView) findViewById(R.id.home_iv_today_task);
@@ -80,7 +83,7 @@ public class HomeActivity extends BaseActivity implements OnClickListener {
 //        ivTodayTask.setPadding(p, p, p, p);
         ivTodayTask.getLayoutParams().width = r;
         ILog.d("r:" + r);
-        ivTodayTask.setBackgroundDrawable(new BitmapDrawable(createRoundBitmap(r)));
+        ivTodayTask.setBackgroundDrawable(new BitmapDrawable(createRoundBitmap(r, 0xcff6efdd)));
         
         header = (Header) findViewById(R.id.home_header);
         header.setTitle("移动医护系统护士端");
@@ -92,29 +95,60 @@ public class HomeActivity extends BaseActivity implements OnClickListener {
                 showOption();
             }
         });
+        
+        
+        findViewById(R.id.home_ll_infusion).setOnClickListener(this);
+        findViewById(R.id.home_ll_drug_verify).setOnClickListener(this);
+        findViewById(R.id.home_ll_advice_perform).setOnClickListener(this);
+        findViewById(R.id.home_ll_nurse_round).setOnClickListener(this);
+        findViewById(R.id.home_ll_patient_info).setOnClickListener(this);
+        findViewById(R.id.home_ll_sign_collect).setOnClickListener(this);
+        findViewById(R.id.home_ll_time_container).setOnClickListener(this);
+        
+        tvTaskNum = (TextView) findViewById(R.id.home_tv_task_num);
+        
+        setTaskNum(32);
+    }
+    
+    private void setTaskNum(int num) {
+        tvTaskNum.setText("" + num);
+        tvTaskNum.measure(0, 0);
+        int d = (int) (Math.max(tvTaskNum.getMeasuredWidth(), tvTaskNum.getMeasuredHeight()) + 5*getResources().getDisplayMetrics().density);
+        Bitmap bm = createRoundBitmap(d, Color.RED);
+        tvTaskNum.setBackground(new BitmapDrawable(bm));
+        
+        tvTaskNum.getLayoutParams().width = d;
+        tvTaskNum.getLayoutParams().height = d;
+        tvTaskNum.setGravity(Gravity.CENTER);
     }
     
     private void showOption() {
-        View view = LayoutInflater.from(this).inflate(R.layout.option, null);
-        view.measure(0, 0);
-        PopupWindow pop = new PopupWindow(view, view.getMeasuredWidth(), view.getMeasuredHeight(), true);
-        pop.setOutsideTouchable(false);
-        pop.setTouchable(true);
-        pop.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-        
-        view.findViewById(R.id.option_ll_change_password).setOnClickListener(this);
-        view.findViewById(R.id.option_ll_setting).setOnClickListener(this);
-        view.findViewById(R.id.option_ll_sign_out).setOnClickListener(this);
+        if (pop == null) {
+            View view = LayoutInflater.from(this).inflate(R.layout.option, null);
+            view.measure(0, 0);
+            pop = new PopupWindow(view, view.getMeasuredWidth(), view.getMeasuredHeight(), true);
+            pop.setOutsideTouchable(false);
+            pop.setTouchable(true);
+            pop.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            
+            view.findViewById(R.id.option_ll_change_password).setOnClickListener(this);
+            view.findViewById(R.id.option_ll_setting).setOnClickListener(this);
+            view.findViewById(R.id.option_ll_sign_out).setOnClickListener(this);
+        }
         
         pop.showAsDropDown(header.getRightView());
     }
+    
+    private void dismissOption() {
+        pop.dismiss();
+    }
 
-    private Bitmap createRoundBitmap(int h) {
-        Bitmap bg = Bitmap.createBitmap(h, h, Config.ARGB_8888);
+    private Bitmap createRoundBitmap(int d, int color) {
+        Bitmap bg = Bitmap.createBitmap(d, d, Config.ARGB_8888);
         Canvas c = new Canvas(bg);
         Paint p = new Paint();
-        p.setColor(0xcff6efdd);
-        c.drawRoundRect(new RectF(new Rect(0, 0, h, h)), h/2, h/2, p);
+        p.setColor(color);
+        c.drawRoundRect(new RectF(new Rect(0, 0, d, d)), d/2, d/2, p);
         return bg;
     }
 
@@ -123,16 +157,24 @@ public class HomeActivity extends BaseActivity implements OnClickListener {
         switch (v.getId()) {
         case R.id.home_iv_today_task:
             ILog.d("今日任务");
-            
             break;
         case R.id.option_ll_change_password:
             ILog.d("改变密码");
+            dismissOption();
             break;
         case R.id.option_ll_setting:
-            
+            dismissOption();
             break;
         case R.id.option_ll_sign_out:
+            dismissOption();
+            break;
             
+        case R.id.home_ll_drug_verify:
+            ILog.d("药品核对");
+            break;
+            
+        case R.id.home_ll_infusion:
+            ILog.d("输液监控");
             break;
 
         default:
